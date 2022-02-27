c3ae2c6e5b8f00115ea7fd4edf299d48a3b15f9e
Vakili <p.vakili@avajang.com>
2022-02-27 17:10:25 +0330

    learnvue

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+    data(){
+        return{
+        isPurple:false,
+        isRed:false,
+        selectedColor:''
+        }
+    },
+    computed:{
+        circleClasses(){
+            return {red:this.isRed}
+        }
+    }
+}
+</script>
+<template>
+<label>
+<input type="checkbox" v-model="isRed"/>red
+
+</label>
+<select v-model="selectedColor">
+<option>white</option>
+<option value="text-black">black</option>
+<option value="text-orange">orange</option>
+</select>
+<div class="circle" :class="[selectedColor,circleClasses]">hi</div>
+</template>
+<style src="../assets/main.css" scoped>
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './assets/style.css'
+import './assets/tailwind.css'
+createApp(App).mount('#app')
